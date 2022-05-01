bc97774dc1ec7cf4d11c21f0153922b586cfed5e
yukuda <leeyookoo@naver.com>
2022-05-01 16:16:32 +0900

    stream FileOutputStreamTest

---
+++ JavaStudy_Chapter15/src/stream/outputstream/FileOutputStreamTest.java
@@ -0,0 +1,25 @@
+package stream.outputstream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest {
+
+	public static void main(String[] args) {
+		
+		byte[] bs = new byte[26];
+		byte data = 65;
+		for(int i = 0; i < bs.length; i++) {
+			bs[i] = data++;
+		}
+		
+		try (FileOutputStream fos = new FileOutputStream("output.txt")) {
+			fos.write(bs);
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		System.out.println("end");
+	}
+
+}
